e1437bd7ee2694327f23aa1b3269713f33b04885
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-18 20:51:50 -0600

    changing the background color when the player wins

--- script.js
+++ script.js
@@ -15,19 +15,24 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
+  //When is there no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+    //When player guess the number
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    //When player guess too high
   } else if (guess > secretNumber && score > 1) {
     document.querySelector('.message').textContent = '📈 Too high!';
     score = score - 1;
     document.querySelector('.score').textContent = score;
+    //When the player guess is too low
   } else if (guess < secretNumber && score > 1) {
     document.querySelector('.message').textContent = '📉 Too low';
     score = score - 1;
     document.querySelector('.score').textContent = score;
+    //When the player run out of tryings
   } else {
     document.querySelector('.message').textContent = 'Game Over 😞';
     document.querySelector('.score').textContent = 0;
