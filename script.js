7643ef5caf4373b3924e387033ed6c98a7449e90
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-18 21:35:02 -0600

    making again button reset the DOM and start all over

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🥳 Correct Number!';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!';
     document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('.number').style.width = '30rem'; //always should equal to string.
     //When player guess too high
